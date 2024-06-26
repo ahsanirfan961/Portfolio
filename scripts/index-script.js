786ae57bb80426e42354f351d031783cb5e46311
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-06-27 01:46:14 +0500

    Added Json support

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -3,6 +3,7 @@ import { optionsButtonListener } from "./header-script.js";
 
 loadDependencies().then(()=>{
     optionsButtonListener();
+    loadData();
 })
 
 function loadHeader() {
@@ -10,6 +11,30 @@ function loadHeader() {
         document.getElementById('header').innerHTML = data;
     }).catch(error => {
         console.error('Error loading: ', error);
+    });
+}
+
+function loadData() {
+    fetch('data.json').then(response => response.json()).then(data => {
+        document.getElementById('name').innerText = data.name;
+        document.getElementById('position-statement').innerText = data.position_statement;
+        document.getElementById('front-page-description').innerText = data.front_page_description;
+        document.getElementById('date-of-birth').innerText = data.date_of_birth;
+        document.getElementById('mobile').innerText = data.mobile;
+        document.getElementById('email').innerText = data.email;
+        document.getElementById('address').innerText = data.address;
+        document.getElementById('facebook').addEventListener('click', function () {
+            window.open(data.facebook, '_blank', 'noopener,noreferrer');
+        });
+        document.getElementById('instagram').addEventListener('click', function () {
+            window.open(data.instagram, '_blank', 'noopener,noreferrer');
+        });
+        document.getElementById('linkedin').addEventListener('click', function () {
+            window.open(data.linkedin, '_blank', 'noopener,noreferrer');
+        });
+        document.getElementById('github').addEventListener('click', function () {
+            window.open(data.github, '_blank', 'noopener,noreferrer');
+        });
     });
 }
 
